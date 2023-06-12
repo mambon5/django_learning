e5d242c98d48fd26b5883dd5fb5c9855b2c3203f
mambon5 <monantoniet@gmail.com>
2023-06-12 07:58:41 +0200

    midway through tutorial 7

--- polls_project/polls/admin.py
+++ polls_project/polls/admin.py
@@ -4,5 +4,14 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+# admin.site.register(Question) # old way of adding question to admin
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None,               {'fields': ['question_text']}),
+            ('Date information', {'fields': ['pub_date']}),
+        ]
+
+admin.site.register(Question, QuestionAdmin)
+
 admin.site.register(Choice)
