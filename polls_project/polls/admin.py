0579cfbc31e2cd232e1ad95e9cda8013efcfdc46
mambon5 <monantoniet@gmail.com>
2023-06-18 16:28:01 +0200

    advanced in tutorial 7

--- polls_project/polls/admin.py
+++ polls_project/polls/admin.py
@@ -6,12 +6,22 @@ from .models import Question, Choice
 
 # admin.site.register(Question) # old way of adding question to admin
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-            (None,               {'fields': ['question_text']}),
-            ('Date information', {'fields': ['pub_date']}),
-        ]
+    # fieldsets = [
+    #     (None,               {'fields': ['question_text']}),
+    #     ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    # ]
+    # inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    inlines = [ChoiceInline]
+
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
 
-admin.site.register(Choice)
+# admin.site.register(Choice)
--- polls_project/polls/models.py
+++ polls_project/polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
@@ -10,9 +11,18 @@ class Question(models.Model):
     def __str__(self):
         return "{} : {}".format(self.question_text, self.pub_date)
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+
     def was_published_recently(self):
         now = timezone.now()
         return now >= self.pub_date >= now - datetime.timedelta(days=1) 
+    
+    
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
